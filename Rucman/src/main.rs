29fca832f825a1f9c655688682ff63ddd7865443
Hans Landgrebe <hlxbl20@gmail.com>
2025-05-07 10:05:33 -0500

    Created grid
    The grid is a 2d vector of GridPoints. GridPoints is a enum that states what is stored in that part of the grid. It also provides what symbol represents the grid point.

--- Rucman/src/main.rs
+++ Rucman/src/main.rs
@@ -1,3 +1,72 @@
+struct Point(u32, u32);
+
+#[derive(Debug, Clone, Copy)]
+enum GridPoint {
+    Pellet,
+    PowerPellet,
+    Wall,
+    Empty
+}
+
+impl From<GridPoint> for String {
+    fn from(input: GridPoint) -> String {
+        match input {
+            GridPoint::Pellet => String::from("."),
+            GridPoint::PowerPellet => String::from("*"),
+            GridPoint::Wall => String::from("|"),
+            GridPoint::Empty => String::from(" "),
+        }
+    }
+}
+
+impl From<GridPoint> for char {
+    fn from(input: GridPoint) -> char {
+        match input {
+            GridPoint::Pellet => '.',
+            GridPoint::PowerPellet => '*',
+            GridPoint::Wall => '|',
+            GridPoint::Empty => ' ',
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Grid {
+    grid: Vec<Vec<GridPoint>>,
+    width: usize,
+    height: usize,
+}
+
+impl Grid {
+    fn new() -> Self {
+        let grid = vec![
+            vec![GridPoint::Wall, GridPoint::Wall, GridPoint::Wall, GridPoint::Wall, GridPoint::Wall],
+            vec![GridPoint::Wall, GridPoint::Pellet, GridPoint::PowerPellet, GridPoint::Pellet, GridPoint::Wall],
+            vec![GridPoint::Wall, GridPoint::Pellet, GridPoint::Empty, GridPoint::Pellet, GridPoint::Wall],
+            vec![GridPoint::Wall, GridPoint::Empty, GridPoint::Empty, GridPoint::Empty, GridPoint::Wall],
+            vec![GridPoint::Wall, GridPoint::Wall, GridPoint::Wall, GridPoint::Wall, GridPoint::Wall],
+        ];
+        
+        Grid {
+            width: grid[0].len(),
+            height: grid.len(),
+            grid,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let grid = Grid::new();
+    println!("{grid:?}");
+    print_screen(&grid);
+}
+
+fn print_screen(grid: &Grid) {
+    for row in &grid.grid {
+        let mut row_string = String::from("");
+        for col in row {
+            row_string.push(char::from(*col));
+        }
+        print!("{row_string}\n");
+    }
 }
