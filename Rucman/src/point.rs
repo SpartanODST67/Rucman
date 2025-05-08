7c24d4e806191ca2b0dc44082b45cdfc91ef3d4a
Hans Landgrebe <hlxbl20@gmail.com>
2025-05-08 12:50:57 -0500

    added Vector2 addition

--- Rucman/src/point.rs
+++ Rucman/src/point.rs
@@ -1,1 +1,30 @@
+use std::ops::Add;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Vector2(pub i32, pub i32);
+
+impl Add for Vector2 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self (
+            self.0 + rhs.0,
+            self.1 + rhs.1
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(Vector2(0, 0) + Vector2(1, 1), Vector2(1, 1));
+        assert_eq!(Vector2(1, 1) + Vector2(0, 0), Vector2(1, 1));
+        assert_eq!(Vector2(0, 0) + Vector2(-1, -1), Vector2(-1, -1));
+        assert_eq!(Vector2(-1, -1) + Vector2(0, 0), Vector2(-1, -1));
+        assert_eq!(Vector2(1, 1) + Vector2(-1, -1), Vector2(0, 0));
+        assert_eq!(Vector2(-1, -1) + Vector2(1, 1), Vector2(0, 0));
+    }
+}
