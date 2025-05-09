c93a89d77434b53b154331a13a0204ba7fec5cd7
Hans Landgrebe <hlxbl20@gmail.com>
2025-05-09 11:49:00 -0500

    Created forward() and back()
    Returns a vector forwards or backwards of the vector relative to the provided direction.

--- Rucman/src/point.rs
+++ Rucman/src/point.rs
@@ -1,5 +1,6 @@
 use std::ops::{Add, Sub};
 
+use crate::direction::Direction;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Vector2(pub i32, pub i32);
@@ -26,6 +27,26 @@ impl Sub for Vector2 {
     }
 }
 
+impl Vector2 {
+    pub fn forward(&self, direction: Direction) -> Vector2 {
+        match direction {
+            Direction::Up(pos) |
+            Direction::Down(pos) |
+            Direction::Left(pos) |
+            Direction::Right(pos) => pos + *self,
+        }
+    }
+
+    pub fn back(&self, direction: Direction) -> Vector2 {
+        match direction {
+            Direction::Up(pos) | 
+            Direction::Down(pos) | 
+            Direction::Left(pos) | 
+            Direction::Right(pos) => *self - pos,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,5 +69,35 @@ mod tests {
         assert_eq!(Vector2(-1, -1) - Vector2(0, 0), Vector2(-1, -1));
         assert_eq!(Vector2(1, 1) - Vector2(-1, -1), Vector2(2, 2));
         assert_eq!(Vector2(-1, -1) - Vector2(1, 1), Vector2(-2, -2));
+    }
+
+    #[test]
+    fn test_forward() {
+        let mut origin = Vector2(0, 0);
+        origin = origin.forward(Direction::up());
+        assert_eq!(origin, Vector2(0, -1));
+        origin = origin.forward(Direction::left());
+        assert_eq!(origin, Vector2(-1, -1));
+        origin = origin.forward(Direction::down());
+        assert_eq!(origin, Vector2(-1, 0));
+        origin = origin.forward(Direction::right());
+        assert_eq!(origin, Vector2(0, 0));
+        origin = origin.forward(Direction::up()).forward(Direction::up());
+        assert_eq!(origin, Vector2(0, -2));
+    }
+
+    #[test]
+    fn test_back() {
+        let mut origin = Vector2(0, 0);
+        origin = origin.back(Direction::up());
+        assert_eq!(origin, Vector2(0, 1));
+        origin = origin.back(Direction::left());
+        assert_eq!(origin, Vector2(1, 1));
+        origin = origin.back(Direction::down());
+        assert_eq!(origin, Vector2(1, 0));
+        origin = origin.back(Direction::right());
+        assert_eq!(origin, Vector2(0, 0));
+        origin = origin.back(Direction::up()).back(Direction::up());
+        assert_eq!(origin, Vector2(0, 2));
     }
 }
