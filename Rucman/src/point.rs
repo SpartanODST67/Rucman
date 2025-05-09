a62827c142ea5b3778351f1613490f6382f9440b
Hans Landgrebe <hlxbl20@gmail.com>
2025-05-09 11:41:13 -0500

    Added vector subtraction

--- Rucman/src/point.rs
+++ Rucman/src/point.rs
@@ -1,4 +1,5 @@
-use std::ops::Add;
+use std::ops::{Add, Sub};
+
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Vector2(pub i32, pub i32);
@@ -10,6 +11,17 @@ impl Add for Vector2 {
         Self (
             self.0 + rhs.0,
             self.1 + rhs.1
+        )
+    }
+}
+
+impl Sub for Vector2 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self (
+            self.0 - rhs.0,
+            self.1 - rhs.1,
         )
     }
 }
@@ -26,5 +38,15 @@ mod tests {
         assert_eq!(Vector2(-1, -1) + Vector2(0, 0), Vector2(-1, -1));
         assert_eq!(Vector2(1, 1) + Vector2(-1, -1), Vector2(0, 0));
         assert_eq!(Vector2(-1, -1) + Vector2(1, 1), Vector2(0, 0));
+    }
+
+    #[test]
+    fn test_sub() {
+        assert_eq!(Vector2(0, 0) - Vector2(1, 1), Vector2(-1, -1));
+        assert_eq!(Vector2(1, 1) - Vector2(0, 0), Vector2(1, 1));
+        assert_eq!(Vector2(0, 0) - Vector2(-1, -1), Vector2(1, 1));
+        assert_eq!(Vector2(-1, -1) - Vector2(0, 0), Vector2(-1, -1));
+        assert_eq!(Vector2(1, 1) - Vector2(-1, -1), Vector2(2, 2));
+        assert_eq!(Vector2(-1, -1) - Vector2(1, 1), Vector2(-2, -2));
     }
 }
